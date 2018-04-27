32718be2beddcfde1bfacba1ba2b65816d5aebf3
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2018-04-26 22:34:19 -0500

    Delete add_buttons.h

--- src/add_buttons.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "const.h"
-#include "button.h"
-//helper class only used to add buttons into a level; due to the sheer amount of buttons it was separated from main and placed here.
-void add_buttons(AllButtons& buttons_in_level);
