8dd5c303efe91a8daa50465181cf7e10ae47f34f
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2018-05-02 00:48:37 -0500

    Delete add_buttons_text.h

--- src/add_buttons_text.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "const.h"
-#include "button.h"
-#include "gametext.h"
-//helper class only used to add buttons and text into a level; due to the sheer amount of buttons and text it was separated from main and placed here.
-void add_buttons_text(AllButtons& buttons_in_level, AllText& text_in_level);
