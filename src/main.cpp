a5b03e798764835643467e2a03fc70bf22412944
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2018-05-02 23:13:50 -0500

    added explanation for usage of borderless window

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 
 //========================================================================
 int main( ){
+	//sets the window to borderless windowed.
+	//this was done because of weird coordinate issues with openFrameworks;
+	//in a bordered window, the 0 y coordinate for in-game drawing is located at the
+	//top of the window hidden under the border while the 0 y coordinate for the mouse
+	//starts at the bottom of the border, causing undesirable discrepancy between
+	//the mouse position as it appears in game and where the mouse actually is.
 	ofGLFWWindowSettings settings;
 	settings.width = kLevelWidthMultiplier * kWallWidth;
 	settings.height = kLevelHeightMultiplier * kWallWidth;
